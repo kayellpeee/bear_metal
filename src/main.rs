4a8c17e62d32a4273d12baee034cfc4c28f33697
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-13 22:59:54 -0700

    [feat] runs bare bones #[start] fn with cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+// very minimal rust program
+#![crate_type="lib"]
+#![feature(lang_items, start, no_std)]
+#![no_std]
+
+// http://stackoverflow.com/questions/28649120/
+// unable-to-compile-rust-with-no-std-lang-items
+#[lang="phantom_fn"]
+trait PhantomFn<A: ?Sized, R: ?Sized = ()> {}
+
+#[lang="sized"]
+trait Sized: PhantomFn<Self> {}
+
+#[lang="copy"]
+trait Copy: PhantomFn<Self> {}
+
+#[lang="sync"]
+trait Sync: PhantomFn<Self> {}
+
+#[start]
+fn start(_argc: isize, _argv: *const *const u8) -> isize {
+    0
 }
+
+
