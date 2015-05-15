2214ce841ac9dc96d5c4c2de64a2d68294bd62c0
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-14 19:56:13 -0700

    [feat] passes command line args to #[start] fn

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 // very minimal rust program
-#![crate_type="lib"]
 #![feature(start)]
-#![feature(lang_items)]
-#![feature(no_std)]
-#![no_std]
-
-extern crate syscall;
-use syscall::*;
 
 #[start]
 fn start(_argc: isize, _argv: *const *const u8) -> isize {
-    syscall::WRITE("test");
+    println!("this is a test {:?} {:?}", _argv, _argc);
     0
 }
 
-#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
