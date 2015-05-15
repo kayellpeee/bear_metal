ebbbf91c52f0a776edc41748cfdc30418f207ba6
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-14 21:40:30 -0700

    [feat] adds necessary lang_items

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 // very minimal rust program
-#![crate_type="lib"]
-#![feature(start, no_std, core)]
+#![feature(lang_items, start, no_std, core)]
 #![no_std]
-
-#[macro_use]
-extern crate core;
 
 #[start]
 fn start(_argc: isize, _argv: *const *const u8) -> isize {
-    assert_eq!(_argc, 1);
     0
 }
 
+#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang="panic_fmt"]
+pub fn panic_fmt(_fmt: &core::fmt::Arguments,
+                 _file_line: &(&'static str, usize)) -> ! {
+    loop { }
+}
